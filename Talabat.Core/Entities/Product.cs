a90a3f78f48fb4544bd600a169e25e3a6a1a88ec
HP <HP@NOHA>
2024-12-02 19:54:16 +0200

    Add project files.

---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+    public class Product:BaseEntity
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Discription { get; set; }
+        public string PictureURL { get; set; }
+        public ProductBrand ProductBrand{ get; set; }
+        public int ProductBrandId { get; set; }
+        public ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+
+
+    }
+}
